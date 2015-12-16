12bd0edfad5af81267e9ab84d0f7827f4f68e754
Angel Chang <angelx@gmail.com>
2015-12-15 21:16:51 -0800

    Set version to 0.1.0

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "shapenet-viewer"
 
-version := "1.0-SNAPSHOT"
+version := "0.1.0"
 
 scalaVersion := "2.11.7"
 
--- scripts/vars.sh
+++ scripts/vars.sh
@@ -17,18 +17,10 @@ export SHAPENET_VIEWER_DIR=$BASE/
 
 # Built jar with all dependencies (and main class is edu.stanford.graphics.shapenet.jme3.viewer.Viewer)
 SHAPENET_VIEWER_TARGET_DIR=$BASE/target/scala-2.11/
-SHAPENET_VIEWER_JAR=$SHAPENET_VIEWER_TARGET_DIR/shapenet-viewer-assembly-1.0-SNAPSHOT.jar
+SHAPENET_VIEWER_JAR=$SHAPENET_VIEWER_TARGET_DIR/shapenet-viewer-assembly-0.1.0.jar
 
-SHAPENET_VIEWER_DEPS_JAR=$SHAPENET_VIEWER_TARGET_DIR/shapenet-viewer-assembly-1.0-SNAPSHOT-deps.jar
+SHAPENET_VIEWER_DEPS_JAR=$SHAPENET_VIEWER_TARGET_DIR/shapenet-viewer-assembly-0.1.0-deps.jar
 SHAPNET_VIEWER_CLASSES=$SHAPENET_VIEWER_TARGET_DIR/classes
-
-# Data dir for shapenet data on dovahkiin
-#export DATA_DIR=/mnt/etherion/data
-if [ `hostname` = "dovahkiin" ] ; then
-  export DATA_DIR=/media/data
-  export USE_LOCAL_DATA=true
-  echo "Using local data with DATA_DIR=$DATA_DIR"
-fi
 
 # Classpath
 CLASSPATH=$SHAPENET_VIEWER_JAR
